4fe8ab04e3f4f597df4e9dcacfacf4d83611c1b3
ayung <ayungpei@gmail.com>
2021-07-20 00:43:59 +0800

    Initial Commit

---
+++ JapaneseGreatWriter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JapaneseGreatWriter
+//
+//  Created by apple on 2021/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
